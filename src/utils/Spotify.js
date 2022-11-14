7d27109be27f51fb6b9a1dd289aeb2c41f656c0b
sarapoul <pormee2001@gmail.com>
2022-11-15 04:06:36 +0700

    ch redirect_uri

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,6 +1,6 @@
 const client_id = 'a6e27d20a21a4785bdfe16d428d6eb41'
 // const redirect_uri = 'http://localhost:3000/'
-const redirect_uri = 'https://assessment-jammming-g6261d3hu-sarapoul.vercel.app/'
+const redirect_uri = 'https://assessment-jammming-jvpu.vercel.app/'
 
 let accessToken
 // let expires_in = undefined
